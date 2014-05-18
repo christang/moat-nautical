3bf6102d4370c0274323fb32168aa47d712011dc
Chris Tang <chris.l.tang@gmail.com>
2014-05-18 17:13:01 -0400

    Add csv library

--- src/airports/routes/users.js
+++ src/airports/routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var csv = require('csv');
+var fs = require('fs');
+
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource');
